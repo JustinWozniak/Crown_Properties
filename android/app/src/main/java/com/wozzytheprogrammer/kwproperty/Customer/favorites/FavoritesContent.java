d23990e7d2494bb2f47410bbf2527f25090406ca
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-11-22 14:39:54 -0500

    Used getJson method to retrieve favorites. Shows EVERYTHING right now, including dummy data

--- android/app/src/main/java/com/wozzytheprogrammer/kwproperty/Customer/favorites/FavoritesContent.java
+++ android/app/src/main/java/com/wozzytheprogrammer/kwproperty/Customer/favorites/FavoritesContent.java
@@ -1,5 +1,6 @@
 package com.wozzytheprogrammer.kwproperty.Customer.favorites;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -11,6 +12,14 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,8 +38,8 @@ public class FavoritesContent {
      */
     public static final Map<String, FavoriteItem> ITEM_MAP = new HashMap<String, FavoriteItem>();
 
-    private static final int COUNT = 25;
-    private int propertyId;
+    private static final int COUNT = 23;
+
 
     static {
         // Add some sample items.
@@ -45,18 +54,21 @@ public class FavoritesContent {
     }
 
     private static FavoriteItem gatherFavoriteProperties(int position) {
-
+        final String[] propertyIds = new String[100];
         String customersId = FirebaseAuth.getInstance().getCurrentUser().getUid();
         DatabaseReference customersFavList = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(customersId).child("favoriteProperties");
         customersFavList.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    Log.e("DataSnapshot", String.valueOf(dataSnapshot));
-                    if (dataSnapshot.hasChild("favoriteProperties")) {
+                    for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+
+//                        Log.e("key", snapshot.getKey());
+//                        Log.e("val", (String) snapshot.getValue());
+                        propertyIds[0] = snapshot.getKey();
 
                     }
-                }
+                    getJSON("https://www.wozzytheprogrammer.com/onlineapi.php",propertyIds[0]);                }
             }
 
             @Override
@@ -99,5 +111,106 @@ public class FavoritesContent {
         }
 
 
+    }
+
+
+    private static void getJSON(final String urlWebService, String passedFavId) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+//                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    parseJson(s,passedFavId);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+
+
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+
+
+    }
+    /**
+     Parses the Json when its returned, and sets markers on the maps
+     */
+    private static void parseJson(String json, String favoriteId) throws JSONException {
+        //creating a json array from the json string
+        JSONArray addressArray = new JSONArray(json);
+
+        //creating a string array for listview
+        String[] ids = new String[addressArray.length()];
+        String[] addresses = new String[addressArray.length()];
+        String[] urlString = new String[addressArray.length()];
+        final String[] propertyInformation = new String[addressArray.length()];
+
+
+        ITEMS.clear();
+        ITEM_MAP.clear();
+        //looping through all the elements in json array
+        for (int i = 0; i < addressArray.length(); i++) {
+
+            JSONObject obj = addressArray.getJSONObject(i);
+            ids[i] = obj.getString("id");
+            addresses[i] = obj.getString("address");
+            propertyInformation[i] = obj.getString("information");
+            urlString[i] = obj.getString("urlString");
+
+            FavoriteItem newestItem = new FavoriteItem(ids[i],addresses[i],propertyInformation[i]);
+
+
+            ITEMS.add(newestItem);
+            ITEM_MAP.put(newestItem.id, newestItem);
+            Log.e("ITEMS", String.valueOf(ITEMS));
+            Log.e("ITEM_MAP", String.valueOf(ITEM_MAP));
+
+        }
     }
 }
