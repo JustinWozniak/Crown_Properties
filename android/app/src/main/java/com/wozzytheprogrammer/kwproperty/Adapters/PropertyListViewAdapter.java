196ee15bb3a68679af2605b0e4dd6ac3f2f903ab
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-12-04 12:10:41 -0500

    Removing favorites from databse now works when you double click the heart

--- android/app/src/main/java/com/wozzytheprogrammer/kwproperty/Adapters/PropertyListViewAdapter.java
+++ android/app/src/main/java/com/wozzytheprogrammer/kwproperty/Adapters/PropertyListViewAdapter.java
@@ -36,13 +36,12 @@ public class PropertyListViewAdapter extends RecyclerView.Adapter<BaseViewHolder
     public static final int VIEW_TYPE_EMPTY = 0;
     public static final int VIEW_TYPE_NORMAL = 1;
 
-    DatabaseReference mCustomer;
+    int position = 0;
+
+    private static int clickCount = 0;
 
     private Callback mCallback;
     private List<Properties> mPropertiesList;
-
-
-    private Boolean isAFavProperty = false;
 
 
     public PropertyListViewAdapter(List<Properties> propertiesList) {
@@ -94,7 +93,7 @@ public class PropertyListViewAdapter extends RecyclerView.Adapter<BaseViewHolder
         if (mPropertiesList != null && mPropertiesList.size() > 0) {
             return mPropertiesList.size();
         } else {
-            return 1;
+            return 0;
         }
     }
 
@@ -106,190 +105,166 @@ public class PropertyListViewAdapter extends RecyclerView.Adapter<BaseViewHolder
     }
 
 
-public interface Callback {
-    void onEmptyViewRetryClick();
-}
-
-
-public class ViewHolder extends BaseViewHolder {
-
-    @BindView(R.id.property_added_text)
-    TextView propertyAddedToFavs;
-
-    @BindView(R.id.thumbnail)
-    ImageView coverImageView;
-
-    @BindView(R.id.title)
-    TextView titleTextView;
-
-    @BindView(R.id.newsTitle)
-    TextView newsTextView;
-
-    @BindView(R.id.newsInfo)
-    TextView infoTextView;
-
-    @BindView(R.id.idTextView)
-    TextView idTextView;
-
-    @BindView(R.id.button_favorite)
-    Button buttonFavorite;
-
-    public void scaleView(View v, float startScale, float endScale) {
-        Animation anim = new ScaleAnimation(
-                1f, 1f, // Start and end values for the X axis scaling
-                startScale, endScale, // Start and end values for the Y axis scaling
-                Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
-                Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
-        anim.setFillAfter(true); // Needed to keep the result of the animation
-        anim.setDuration(500);
-        v.startAnimation(anim);
+    public interface Callback {
+        void onEmptyViewRetryClick();
     }
 
 
-    public ViewHolder(View itemView) {
-        super(itemView);
-        ButterKnife.bind(this, itemView);
+    public class ViewHolder extends BaseViewHolder {
 
-        DatabaseReference mUser;
-        mUser = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(FirebaseAuth.getInstance().getUid());
+        @BindView(R.id.property_added_text)
+        TextView propertyAddedToFavs;
 
-        DatabaseReference favoritePropertiesRef = mUser.child("favoriteProperties");
-        Map<String, Object> favoritesUpdates = new TreeMap<>();
+        @BindView(R.id.thumbnail)
+        ImageView coverImageView;
+
+        @BindView(R.id.title)
+        TextView titleTextView;
+
+        @BindView(R.id.newsTitle)
+        TextView newsTextView;
+
+        @BindView(R.id.newsInfo)
+        TextView infoTextView;
+
+        @BindView(R.id.idTextView)
+        TextView idTextView;
+
+        @BindView(R.id.button_favorite)
+        Button buttonFavorite;
+
+        public void scaleView(View v, float startScale, float endScale) {
+            Animation anim = new ScaleAnimation(
+                    1f, 1f, // Start and end values for the X axis scaling
+                    startScale, endScale, // Start and end values for the Y axis scaling
+                    Animation.RELATIVE_TO_SELF, 0f, // Pivot point of X scaling
+                    Animation.RELATIVE_TO_SELF, 1f); // Pivot point of Y scaling
+            anim.setFillAfter(true); // Needed to keep the result of the animation
+            anim.setDuration(500);
+            v.startAnimation(anim);
+        }
 
 
-        buttonFavorite.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.N)
-            @Override
-            public void onClick(View view) {
-                scaleView(buttonFavorite, 1, 3);
-                scaleView(buttonFavorite, 2, 1);
-                if (!isAFavProperty) {
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+
+        }
+
+        protected void clear() {
+            coverImageView.setImageDrawable(null);
+            titleTextView.setText("");
+            newsTextView.setText("");
+            infoTextView.setText("");
+            idTextView.setText("");
+
+        }
+
+
+        public void onBind(int position) {
+            super.onBind(position);
+
+            final Properties mProperties = mPropertiesList.get(position);
+
+            if (mProperties.getImageUrl() != null) {
+                Glide.with(itemView.getContext())
+                        .load(mProperties.getImageUrl())
+                        .into(coverImageView);
+            }
+
+            if (mProperties.getTitle() != null) {
+                titleTextView.setText(mProperties.getTitle());
+            }
+
+            if (mProperties.getSubTitle() != null) {
+                newsTextView.setText(mProperties.getSubTitle());
+            }
+
+            if (mProperties.getInfo() != null) {
+                infoTextView.setText(mProperties.getInfo());
+            }
+            if (mProperties.getId() != null) {
+                idTextView.setText(mProperties.getId());
+            }
+
+
+            buttonFavorite.setOnClickListener(new View.OnClickListener() {
+
+                DatabaseReference mUser = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(FirebaseAuth.getInstance().getUid());
+
+                DatabaseReference favoritePropertiesRef = mUser.child("favoriteProperties");
+                Map<String, Object> favoritesUpdates = new TreeMap<>();
+
+
+                @RequiresApi(api = Build.VERSION_CODES.N)
+                @Override
+                public void onClick(View view) {
+
+                    clickCount++;
+
+
+                    Log.e("view", view.toString());
+                    scaleView(buttonFavorite, 1, 3);
+                    scaleView(buttonFavorite, 2, 1);
                     propertyAddedToFavs.setText(R.string.is_a_favorite);
                     propertyAddedToFavs.setVisibility(View.VISIBLE);
-                    isAFavProperty = true;
+
                     favoritesUpdates.put((String) idTextView.getText(), titleTextView.getText());
-
+                    Log.e("dsds", favoritesUpdates.toString());
                     favoritePropertiesRef.updateChildren(favoritesUpdates);
-                } else {
-                    propertyAddedToFavs.setVisibility(View.GONE);
-                    isAFavProperty = false;
+                    String key = String.valueOf(idTextView.getText());
+
+                    if (clickCount > 1) {
+                        propertyAddedToFavs.setText(R.string.is_not_a_favorite);
+
+                        favoritesUpdates.remove((String) idTextView.getText(), titleTextView.getText());
+                        Log.e("vifdsew", favoritesUpdates.toString());
+                        favoritePropertiesRef.child(key).removeValue();
+                        clickCount = 0;
+                    }
+
 
                 }
-            }
-        });
-    }
+            });
 
-    protected void clear() {
-        coverImageView.setImageDrawable(null);
-        titleTextView.setText("");
-        newsTextView.setText("");
-        infoTextView.setText("");
-        idTextView.setText("");
 
-    }
+            itemView.setOnClickListener(v -> {
 
-    public void onBind(int position) {
-        super.onBind(position);
 
-        final Properties mProperties = mPropertiesList.get(position);
-
-        if (mProperties.getImageUrl() != null) {
-            Glide.with(itemView.getContext())
-                    .load(mProperties.getImageUrl())
-                    .into(coverImageView);
-        }
-
-        if (mProperties.getTitle() != null) {
-            titleTextView.setText(mProperties.getTitle());
-        }
-
-        if (mProperties.getSubTitle() != null) {
-            newsTextView.setText(mProperties.getSubTitle());
-        }
-
-        if (mProperties.getInfo() != null) {
-            infoTextView.setText(mProperties.getInfo());
-        }
-        if (mProperties.getId() != null) {
-            idTextView.setText(mProperties.getId());
-        }
-
-        itemView.setOnClickListener(v -> {
-            if (mProperties.getImageUrl() != null) {
-                try {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.addCategory(Intent.CATEGORY_BROWSABLE);
-                    intent.setData(Uri.parse(mProperties.getImageUrl()));
-                    itemView.getContext().startActivity(intent);
-                } catch (Exception e) {
-                    Log.e(TAG, "onClick: Image url is not correct");
+                if (mProperties.getImageUrl() != null) {
+                    try {
+                        Intent intent = new Intent();
+                        intent.setAction(Intent.ACTION_VIEW);
+                        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                        intent.setData(Uri.parse(mProperties.getImageUrl()));
+                        itemView.getContext().startActivity(intent);
+                    } catch (Exception e) {
+                        Log.e(TAG, "onClick: Image url is not correct");
+                    }
                 }
-            }
-        });
-    }
-}
-
-public class EmptyViewHolder extends BaseViewHolder {
-
-    @BindView(R.id.tv_message)
-    TextView messageTextView;
-    @BindView(R.id.buttonRetry)
-    TextView buttonRetry;
-
-    EmptyViewHolder(View itemView) {
-        super(itemView);
-        ButterKnife.bind(this, itemView);
-        buttonRetry.setOnClickListener(v -> mCallback.onEmptyViewRetryClick());
+            });
+        }
     }
 
-    @Override
-    protected void clear() {
+
+    public class EmptyViewHolder extends BaseViewHolder {
+
+        @BindView(R.id.tv_message)
+        TextView messageTextView;
+        @BindView(R.id.buttonRetry)
+        TextView buttonRetry;
+
+        EmptyViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            buttonRetry.setOnClickListener(v -> mCallback.onEmptyViewRetryClick());
+        }
+
+        @Override
+        protected void clear() {
+
+        }
 
     }
-
-}
-//
-//    private void getPropertyInformation() {
-//        DatabaseReference propertyReference = FirebaseDatabase.getInstance().getReference().child("Properties").child("Id");
-//        final long[] numberOfProperties = {0};
-//
-//        propertyReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//
-//                if (dataSnapshot.exists()) {
-//                    numberOfProperties[0] = dataSnapshot.getChildrenCount();
-//                    int propertyCount = -1;
-//
-//                    String[] addresses = new String[(int) numberOfProperties[0]];
-//                    String[] imageUrlString = new String[(int) numberOfProperties[0]];
-//                    String[] propertyInformation = new String[(int) numberOfProperties[0]];
-//
-//                    mPropertiesList.clear();
-//
-//                    for (DataSnapshot child : dataSnapshot.getChildren()) {
-//                            propertyCount++;
-//
-//                            String key = child.getKey();
-//                            addresses[propertyCount] = String.valueOf(propertyReference.child(String.valueOf(propertyCount)).child("Address"));
-//                            imageUrlString[propertyCount] = String.valueOf(propertyReference.child(String.valueOf(propertyCount)).child("ImgUrl"));
-//                            propertyInformation[propertyCount] = String.valueOf(propertyReference.child(String.valueOf(propertyCount)).child("Information"));
-//
-//                            Properties properties666 = new Properties(imageUrlString[propertyCount], propertyInformation[propertyCount], propertyInformation[propertyCount], addresses[propertyCount], key);
-//
-//                            mPropertiesList.add(properties666);
-//
-//                            }
-//                        }
-//
-//                }
-//
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-//    }
 }
