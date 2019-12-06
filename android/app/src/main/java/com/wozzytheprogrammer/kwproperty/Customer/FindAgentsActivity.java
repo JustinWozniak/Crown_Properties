2af340999eac07bef697a66e03f8be18af67a97a
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-12-06 14:01:00 -0500

    Succesfully done query call to find online agents inside MAX_SEARCH_DISTANCE

--- android/app/src/main/java/com/wozzytheprogrammer/kwproperty/Customer/FindAgentsActivity.java
+++ android/app/src/main/java/com/wozzytheprogrammer/kwproperty/Customer/FindAgentsActivity.java
@@ -10,7 +10,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
 import com.firebase.geofire.GeoQuery;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -19,6 +22,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.wozzytheprogrammer.kwproperty.R;
+
+import java.util.Map;
 
 public class FindAgentsActivity extends AppCompatActivity {
     private ProgressDialog loadingBar;
@@ -54,41 +59,97 @@ public class FindAgentsActivity extends AppCompatActivity {
 
     private int radius = 1;
     private Boolean agentFound = false;
-
+    int MAX_SEARCH_DISTANCE = 1000;
+    boolean requestBol = false;
 
     GeoQuery geoQuery;
 
     private void findAnAgent() {
+
+
         loadingBar.setTitle("Locating Real Estate Agent...");
         loadingBar.setMessage("Please wait, we are scanning your location...");
         loadingBar.show();
 
         String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference customerRef = FirebaseDatabase.getInstance().getReference("Users/Customers/" + uid + "/location");
-
+        DatabaseReference customerLocationRef = FirebaseDatabase.getInstance().getReference("Users/Customers/" + uid + "/location");
 
         DatabaseReference agentLocation = FirebaseDatabase.getInstance().getReference().child("agentsAvailable");
 
-        customerRef.addValueEventListener(new ValueEventListener() {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users/Customers/" + uid + "/location");
+        GeoFire geoFire = new GeoFire(ref);
+
+        customerLocationRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     for (DataSnapshot child : dataSnapshot.getChildren()) {
-                        String  customersLat = String.valueOf(dataSnapshot.child("Lat:").getValue());
-                        Log.e("lat",customersLat);
-                        String  customersLong = String.valueOf(dataSnapshot.child("Long:").getValue());
-                        Log.e("customersLong",customersLong);
-                     
+                        Double customersLat = (Double) dataSnapshot.child("Lat:").getValue();
+                        Double customersLong = (Double) dataSnapshot.child("Long:").getValue();
+                        if (customersLat != null) {
+                            GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(customersLat, customersLong), MAX_SEARCH_DISTANCE);
+
+
+                            geoQuery.addGeoQueryEventListener(new GeoQueryEventListener() {
+                                @Override
+                                public void onKeyEntered(String key, GeoLocation location) {
+                                    DatabaseReference agentAvailable = FirebaseDatabase.getInstance().getReference().child("agentsAvailable");
+                                    Log.e("agentAvailable", String.valueOf(agentAvailable));
+                                    agentAvailable.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.exists()) {
+                                                Map<String, Object> agentMap = (Map<String, Object>) dataSnapshot.getValue();
+                                                Log.e("MAP", String.valueOf(agentMap));
+
+                                                agentFound = true;
+                                                loadingBar.setTitle("Agent Found!...");
+                                                loadingBar.setMessage("Please wait, while we contact them!...");
+
+                                            }   else    {
+                                                agentFound = false;
+                                                loadingBar.setTitle("No Agents Online!...");
+                                                loadingBar.setMessage("Please Try Again Later!...");
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+                                        }
+                                    });
+                                }
+
+                                @Override
+                                public void onKeyExited(String key) {
+
+                                }
+
+                                @Override
+                                public void onKeyMoved(String key, GeoLocation location) {
+
+                                }
+
+                                @Override
+
+                                public void onGeoQueryReady() {
+                                    Log.e("ready", String.valueOf(MAX_SEARCH_DISTANCE));
+                                }
+
+                                @Override
+                                public void onGeoQueryError(DatabaseError error) {
+                                }
+                            });
+                        }
+
                     }
                 }
-
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
+
         });
 
-    }
-}
+    }}
